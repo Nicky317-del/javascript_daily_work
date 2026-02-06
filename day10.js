4c1d1646b0982c39173054824e5b626e5399a162
Nicky317-del <ratheenicky064@gmail.com>
2026-02-06 12:40:25 +0530

    Add files via upload

---
+++ day10.js
@@ -0,0 +1,13 @@
+// function handleClick(){
+//     const button = document.
+//     getElementById("btn");
+//     button.textContent = "Clicked"
+// }
+
+function handleClick(){
+    document.querySelector("h1").style.
+    color="red"
+    const button =document.
+    getElementById("btn");
+    button.textContent="color changed"
+}
